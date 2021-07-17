5da081527d9ae4dcfc717ee40ba6b85b9f3399ec
fushaolei <1563250958@qq.com>
2021-07-17 22:14:51 +0800

    脑阔疼

--- lib/src/main/java/github/fushaolei/lib/test/base/ARecyclerAdapter.kt
+++ lib/src/main/java/github/fushaolei/lib/test/base/ARecyclerAdapter.kt
@@ -9,10 +9,20 @@ import androidx.recyclerview.widget.RecyclerView
 abstract class ARecyclerAdapter<T, VH : ARecyclerViewHolder>(var dataList: List<T>) :
     RecyclerView.Adapter<VH>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+//    override fun  onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
 //        var view = LayoutInflater.from(parent.context).inflate(getItemLayout(), parent, false)
+//
 //        return VH(view)
-    }
+//    }
 
     abstract fun getItemLayout(): Int
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        convert(holder, dataList[position])
+    }
+
+    abstract fun convert(holder: VH, item: T)
+
+    override fun getItemCount(): Int = dataList.size
+
 }
